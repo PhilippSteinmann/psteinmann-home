f1b9d496f9b50c80b3d1e733e3f485a38063e7fa
Philipp Steinmann <steinmann.philipp@yahoo.com>
2013-02-17 00:40:48 -0500

    Add all files from previous site

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(
+function()
+{
+  $(".logo").hover(
+  function()
+  {
+    var rand = Math.random();
+    rand *= 2;
+    rand = Math.floor(rand);
+
+    var replacement = null;
+
+    if (rand >= 0.5)
+    {
+      replacement = ".excl"
+    }
+    else
+    {
+      replacement = ".quest"
+    }
+    
+    $(".excl").stop().animate({ opacity: 0}, 300);
+    $(".quest").stop().animate({ opacity: 0}, 300,
+    function()
+    {
+      $(replacement).stop().animate({ opacity: 1}, 300);
+    } );
+  }, 
+
+  function()
+  {
+    $(".quest").stop().animate({ opacity: 0}, 300);  
+    $(".excl").stop().animate({ opacity: 0}, 300,
+    function()
+    {
+      $(".excl").stop().animate({ opacity: 1}, 300);
+      $(".quest").stop().animate({ opacity: 1}, 300);
+    } );
+  } );
+} );
