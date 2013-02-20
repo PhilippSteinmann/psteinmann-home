64913865aa4bbf9e734ea1fb4ec123e9d1b26b4c
Philipp Steinmann <steinmann.philipp@yahoo.com>
2013-02-19 21:43:10 -0500

    Add slideshow

--- js/main.js
+++ js/main.js
@@ -37,6 +37,18 @@ function()
       $(".quest").stop().animate({ opacity: 1}, 300);
     } );
   } );
-  
-
 } );
+
+function nextImage()
+{ 
+  console.log(current_img);
+  current_img = (current_img + 1) % images.length;
+  $(".slideshow-img").fadeOut(400,
+  function()
+  {
+    $(".slideshow-img").attr("src", images[current_img].url);
+    $(".slideshow-img").attr("title", images[current_img].caption);
+    $(".slideshow-img").attr("alt", images[current_img].caption);
+    $(".slideshow-img").fadeIn(400);
+  } );
+}
